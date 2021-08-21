1a4cc575033c5771f883861ef426239cb3a31094
Muhsin Sahin <sahinmuhsin2018@gmail.com>
2021-08-21 16:55:16 +0200

    Update page_141.cc

--- page_141.cc
+++ page_141.cc
@@ -4,19 +4,32 @@
 using std::string;
 
 void page_141_Ex_4_6();
+void page_141_Ex_4_4();
 
 int main(int argc, char const *argv[]) {
 
-  page_141_Ex_4_6();
+  page_141_Ex_4_4();
+  // page_141_Ex_4_6();
 
   return 0;
 }
+
+void page_141_Ex_4_4(){
+  /* page 141
+    Exercise 4.4: Parenthesize the following expression to show how it is evaluated. Test
+    your answer by compiling the expression (without parentheses) and printing its result.
+        12 / 3 * 4 + 5 * 15 + 24 % 4 / 2
+  */
+  std::cout<<12 / 3 * 4 + 5 * 15 + 24 % 4 / 2<<std::endl;
+  std::cout<<((((12 / 3) * 4) + (5 * 15)) + (24 % (4 / 2)))<<std::endl;
+}
+
 void page_141_Ex_4_6(){
-  /* Page 141 page 141
+  /* Page 141
     Exercise 4.6: Write an expression to determine whether an int value is even or odd.
   */
     std::cout<<"Please enter a number."<<std::endl;
-    
+
     int i;
 
     while(std::cin>>i && i!='q'){
