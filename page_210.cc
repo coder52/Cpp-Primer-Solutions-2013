bf058e25c15d8294e95f24f4e9d72137a3a5d625
Muhsin Sahin <sahinmuhsin2018@gmail.com>
2021-08-22 16:08:25 +0200

    Update page_210.cc

--- page_210.cc
+++ page_210.cc
@@ -1,5 +1,19 @@
 #include <iostream>
 
+void page_210_6_10(int* x1, int* x2);
+
+int main(int argc, char const *argv[]) {
+  int i = 5;
+  int j = 7;
+  std::cout<< "Before swap i = " << i << " and j = " << j << std::endl;
+
+  page_210_6_10(&i, &j);
+
+  std::cout<< "After swap i = " << i << " and j = " << j <<std::endl;
+
+  return 0;
+}
+
 void page_210_6_10(int* x1, int* x2){
   /* Page 210
   Exercise 6.10: Using pointers, write a function to swap the values of two ints. Test
@@ -8,16 +22,4 @@ void page_210_6_10(int* x1, int* x2){
   int dummy=*x1;
   *x1=*x2;
   *x2=dummy;
-}
-
-int main(int argc, char const *argv[]) {
-  int i = 5;
-  int j = 7;
-  std::cout<<"Before swap i = "<<i<<" and j = "<<j<<std::endl;
-
-  page_210_6_10(&i, &j);
-
-  std::cout<<"After swap i = "<<i<<" and j = "<<j<<std::endl;
-
-  return 0;
 }
