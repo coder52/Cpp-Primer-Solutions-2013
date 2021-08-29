5954e51b9e90217e1805ff6334ed4c7d819b51b9
Muhsin Sahin <sahinmuhsin2018@gmail.com>
2021-08-29 21:04:14 +0200

    Update page_222.cc

--- page_222.cc
+++ page_222.cc
@@ -3,6 +3,14 @@
 
 using std::initializer_list;
 
+void page_222_6_27(initializer_list<int> int_list);
+
+int main(int argc, char const *argv[]) {
+  initializer_list<int> int_list = {1,2,3,4,5,6};
+  page_222_6_27(int_list);
+  return 0;
+}
+
 void page_222_6_27(initializer_list<int> int_list){
   /* Page 222
   Exercise 6.27: Write a function that takes an initializer_list<int> and produces
@@ -11,10 +19,4 @@ void page_222_6_27(initializer_list<int> int_list){
   for(const int* beg =int_list.begin();beg!=int_list.end();beg++){
     std::cout<<*beg<<" ";
   }
-}
-
-int main(int argc, char const *argv[]) {
-  initializer_list<int> int_list = {1,2,3,4,5,6};
-  page_222_6_27(int_list);
-  return 0;
 }
