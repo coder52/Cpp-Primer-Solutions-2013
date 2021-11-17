0632d139de53311af9120ae2f66b7343e8ac4143
Muhsin Sahin <sahinmuhsin2018@gmail.com>
2021-11-17 23:17:03 +0100

    Create page_272_23_24.cc

---
+++ Page_275/page_272_23_24.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "Screen.h"
+
+/* Page 272
+  Exercise 7.23: Write your own version of the Screen class.
+
+  Exercise 7.24: Give your Screen class three constructors: a default constructor; a constructor
+  that takes values for height and width and initializes the contents to hold the
+  given number of blanks; and a constructor that takes values for height, width, and a
+  character to use as the contents of the screen.
+
+  Exercise 7.25: Can Screen safely rely on the default versions of copy and assignment?
+  If so, why? If not, why not?
+*/
+
+int main(int argc, char const *argv[]){
+
+  Screen sc_1;
+  print(std::cout, sc_1);
+
+  Screen sc_2 = {11, 45};
+  print(std::cout, sc_2);
+
+  Screen sc_3(23, 34, 'f');
+  print(std::cout, sc_3);
+
+  Screen sc_x = sc_2;
+  print(std::cout, sc_x);
+
+  sc_x=sc_3;
+  print(std::cout, sc_x);
+
+
+  return 0;
+}
