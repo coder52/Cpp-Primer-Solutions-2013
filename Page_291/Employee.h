4ba6d73bdbf444202dab0c888bd913a66e1ca236
Muhsin Sahin <sahinmuhsin2018@gmail.com>
2021-12-14 22:26:04 +0100

    Update Employee.h

--- Page_291/Employee.h
+++ Page_291/Employee.h
@@ -2,7 +2,14 @@
 #define EMPLOYEE_H
 
 struct Employee{
-  
+  Employee() = default;
+  Employee(std::string id_no):id(id_no){}
+  Employee(std::string i, std::string n):id(i), name(n) {}
+  Employee(std::string i, std::string n, std::string add):
+                                                id(i), name(n), address(add) {}
+  Employee(std::string i, std::string n, std::string add, double slry):
+                                  id(i), name(n), address(add), salary(slry) {}
+
 private:
   std::string id;
   std::string name;
@@ -10,7 +17,5 @@ private:
   double salary;
 
 };
-
-
 
 #endif
