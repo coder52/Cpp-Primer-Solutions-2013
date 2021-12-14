50a4b0f285417128a57becfc5d129f533416efc5
Muhsin Sahin <sahinmuhsin2018@gmail.com>
2021-12-14 22:28:51 +0100

    Update Employee.h

--- Page_291/Employee.h
+++ Page_291/Employee.h
@@ -1,6 +1,14 @@
 #ifndef EMPLOYEE_H
 #define EMPLOYEE_H
 
+/* Page 291
+  Exercise 7.40: Choose one of the following abstractions (or an abstraction of your
+  own choosing). Determine what data are needed in the class. Provide an appropriate
+  set of constructors. Explain your decisions.
+    (a) Book      (b) Date    (c) Employee
+    (d) Vehicle   (e) Object  (f) Tree
+*/
+
 struct Employee{
   Employee() = default;
   Employee(std::string id_no):id(id_no){}
